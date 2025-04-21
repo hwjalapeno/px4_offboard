70d6f371c51ca4f3629d29b68791d95006b6fe17
Varun Raghavendra <raghavendravarun88@gmail.com>
2025-04-21 16:01:19 -0400

    Create test_takeoff.py

---
+++ test_takeoff.py
@@ -0,0 +1,18 @@
+    def takeoff(self, altitude: float, yaw: float = 0.0) -> bool:
+        """
+        Command the vehicle to take off.
+        Uses VEHICLE_CMD_NAV_TAKEOFF (22). Altitude is relative to current home.
+        """
+        self.publish_vehicle_command(
+            command=VehicleCommand.VEHICLE_CMD_NAV_TAKEOFF,
+            param1=0.0,       # minimum pitch (unused)
+            param2=0.0,       # empty
+            param3=0.0,       # empty
+            param4=yaw,       # yaw angle
+            param5=0.0,       # latitude (0 => current)
+            param6=0.0,       # longitude
+            param7=altitude   # altitude in meters
+        )
+        self.get_logger().info(f'Takeoff command sent: altitude={altitude}m, yaw={yaw}rad')
+        time.sleep(0.5)
+        return True
